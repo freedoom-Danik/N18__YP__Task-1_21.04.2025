39b1943f00d9a59631237f233f17aea1c40d8ad7
Aunt D <dzavizistur@gmail.com>
2025-04-21 10:24:15 +0300

    WELL DONE FOR REPORT

--- Program.cs
+++ Program.cs
@@ -1,108 +1,74 @@
 ﻿using static System.Console;
+
 namespace ConsoleApp1
 {
     class LSearch<T>
     {
         private T[] A;
-        public LSearch()
-        {
-            A = null;
-        }
-        public LSearch(T[] _A)
-        {
-            A = (T[])_A.Clone();
-        }
-        public T[] Get()
-        {
-            return A;
-        }
-        public void Set(T[] _A)
-        {
-            A = (T[])_A.Clone();
-        }
+
+        public LSearch() => A = null;
+        public LSearch(T[] _A) => A = (T[])_A.Clone();
+
+        public T[] Get() => A;
+        public void Set(T[] _A) => A = (T[])_A.Clone();
+
         public bool IsItem(T[] A, T key)
         {
-            for (int i = 0; i < A.Length; i++)
-                if (A[i].Equals(key)) // проверка, равны ли значения объектов
+            foreach (var item in A)
+                if (item.Equals(key))
                     return true;
             return false;
         }
-        public bool IsItem(T key)
-        {
-            return IsItem(this.A, key);
-        }
+
+        public bool IsItem(T key) => IsItem(A, key);
+
         public int GetNOccurences(T[] A, T key)
         {
-            int k = 0;
-            for (int i = 0; i < A.Length; i++)
-                if (A[i].Equals(key))
-                    k++;
-            return k;
+            int count = 0;
+            foreach (var item in A)
+                if (item.Equals(key))
+                    count++;
+            return count;
         }
-        public int GetNOccurences(T key)
-        {
-            return GetNOccurences(this.A, key);
-        }
+
+        public int GetNOccurences(T key) => GetNOccurences(A, key);
+
         public int[] GetPositions(T[] A, T key)
         {
-            int[] AP = new int[0]; // вначале в массиве 0 элементов
-            int[] AP2; // вспомогательный массив
+            var positions = new System.Collections.Generic.List<int>();
             for (int i = 0; i < A.Length; i++)
-            {
                 if (A[i].Equals(key))
-                {
-                    try
-                    {
-                        AP2 = new int[AP.Length + 1];
-                        AP.CopyTo(AP2, 0);
-                        AP2[AP.Length] = i;
-                        AP = AP2;
-                    }
-                    catch (Exception e)
-                    {
-                        WriteLine(e.Message);
-                    }
-                }
-            }
-            return AP;
+                    positions.Add(i);
+            return positions.ToArray();
         }
-        public int[] GetPositions(T key)
-        {
-            return GetPositions(this.A, key);
-        }
+
+        public int[] GetPositions(T key) => GetPositions(A, key);
+
         public void Print(string comment)
         {
             WriteLine(comment);
-            for (int i = 0; i < A.Length; i++)
-                Write("{0}   ", A[i]);
-            WriteLine();
+            WriteLine(string.Join("   ", A));
         }
     }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             int[] AI = { 5, 8, 9, -1, 4 };
-            LSearch<int> L1 = new LSearch<int>(AI);
-            if (L1.IsItem(8))
-                WriteLine("Item 8 is in array AI");
-            else
-                WriteLine("Item 8 is not in array AI");
+            var L1 = new LSearch<int>(AI);
+            WriteLine(L1.IsItem(8) ? "Item 8 is in array AI" : "Item 8 is not in array AI");
+
             char[] AC = { 'a', 'b', 'f', 'a', 'a', 'c', 'd', 'i', 'f' };
-            LSearch<char> L2 = new LSearch<char>(AC);
-            int n = L2.GetNOccurences('a');
-            WriteLine("n = {0}", n); // n = 3
-            bool[] AB = { true, true, false, false, true, false, true, true }; // массив
-            LSearch<bool> L3 = new LSearch<bool>(); // вызывается конструктор без параметров
-            L3.Set(AB); // Метод Set() - установить новый массив
-            int[] Positions = L3.GetPositions(true);
-            for (int i = 0; i < Positions.Length; i++)
-            {
-                Write("{0}   ", Positions[i]);
-            }
-            WriteLine();
+            var L2 = new LSearch<char>(AC);
+            WriteLine($"n = {L2.GetNOccurences('a')}");
+
+            bool[] AB = { true, true, false, false, true, false, true, true };
+            var L3 = new LSearch<bool>();
+            L3.Set(AB);
+            WriteLine(string.Join("   ", L3.GetPositions(true)));
+
             ReadLine();
         }
-
     }
 }
